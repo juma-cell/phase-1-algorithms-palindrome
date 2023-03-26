0bf8790439d430d0eb1fc6719163e2e515ddce35
Juma Musa <juma.musa@moringaschool.com>
2023-03-26 16:16:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const lastIndex = word.length - 1 - i;
+    if (word[i] !== word[lastIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+   
+  split to array
+  reverse array.
+  join back to string
+  compare both strings
+  return true if both strings match else false
 */
 
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
